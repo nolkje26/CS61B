d7fb949e567651023cc9b201514e13097cb0768f
Arnoldo <arnoldo@berkeley.edu>
2021-01-26 18:07:32 -0700

    Finished proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -26,6 +26,7 @@ public class NBody{
     public static void main(String[] args) {
         double T = Double.parseDouble(args[0]);
         double dT = Double.parseDouble(args[1]);
+        double time = 0;
         String filename = args[2];
         Body[] bodies = readBodies(filename);
         double radius = readRadius(filename);
@@ -33,11 +34,38 @@ public class NBody{
         StdDraw.setScale(-radius, radius);
         StdDraw.enableDoubleBuffering(); // enables double buffer
         StdDraw.clear(); // clears drawing window
-        StdDraw.picture(0, 0, "images/starfield.jpg");
-        for (Body body : bodies){
-            body.draw();
+
+        while (time <= T){
+            double[] arrXforces = new double[bodies.length];
+            double[] arrYforces = new double[bodies.length];
+
+            for (int i = 0; i < bodies.length; i++){
+                arrXforces[i] = bodies[i].calcNetForceExertedByX(bodies);
+                arrYforces[i] = bodies[i].calcNetForceExertedByY(bodies);
+            }
+
+            for (int i = 0; i < bodies.length; i++){
+                bodies[i].update(dT, arrXforces[i], arrYforces[i]);
+            }
+
+            StdDraw.picture(0, 0, "images/starfield.jpg");
+
+            for (Body body : bodies){
+                body.draw();
+            }
+
+            StdDraw.show();
+            StdDraw.pause(10);
+            time += dT;
         }
-        StdDraw.show();
-        StdDraw.pause(2000);
+
+        StdOut.printf("%d\n", bodies.length);
+        StdOut.printf("%.2e\n", radius);
+        
+        for (int i = 0; i < bodies.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                            bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+                            bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
+        }
     }
 }
